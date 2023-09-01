11156cc92cb8bdd866ce21e16c907464a2514f60
Eugene2204 <xander_7@mail.ru>
2023-09-01 16:18:44 +0300

    2.9 DOM 1. Управление шаблоном из JS, события.

--- hw0.js
+++ hw0.js
@@ -1,3 +1,43 @@
+// person — это объект
+const person = {
+  // name, family, age — это свойства, или ключи, объекта person
+  name: 'Анатолий',
+  age: 29,
+  // Объекты могут быть вложенными, в ключе family лежит объект со свойствами mother, father и children
+  family: {
+     mother: 'Елена',
+     father: 'Антон',
+     // В ключах объекта могут лежать массивы, в ключе children лежат дети Анатолия
+     children: ['Варвара', 'Леонид']
+ } 
+};
+
+// Так мы получаем значение свойства объекта
+console.log(person.name); // => Анатолий
+console.log(person.family.father); // => Антон
+
+// Так мы можем изменить значение свойства в объекте
+person.name = 'Иван'
+console.log(person.name); // => Иван
+
+
+// Так мы вызываем метод join у массива children
+console.log(person.family.children.join(' и ')); // => Варвара и Леонид
+person.family.children.push("Сергей");
+// Так мы подставляем значения в строку с помощью шаблонных строк
+console.log(`Имя: ${person.name}. Дети: ${person.family.children.join(', ')}`); // => Имя: Иван. Дети: Варвара и Леонид
+
+function searchStart(arr, start)  {    //создали функцию с двумя аргументами
+    
+  const string = start.toLowerCase();  // приводим к нижнему регистру строку
+  return  arr.filter((str) => str.toLowerCase().includes(string)); //к массиву применяем метод filter, применяем toLowerCase чтобы не учитывался регистр и startsWith проверить,есть ли такое значение
+     
+}
+
+console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко')); // ['кошка', 'комар']
+console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру')); // ['груша']
+console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино')); // []
+
 let customString = 'abcdef';
 let arrayString = [... customString].reverse().join('');
 console.log(arrayString);
